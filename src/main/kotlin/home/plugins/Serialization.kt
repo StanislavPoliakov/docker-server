ee95f973501182d2fd5ae6978b36a194c4bc9e99
Stanislav Poliakov <Stanislav.Poliakov@gmail.com>
2023-06-06 23:17:06 +0300

    Upgrade до 1.2.3

--- src/main/kotlin/home/plugins/Serialization.kt
+++ src/main/kotlin/home/plugins/Serialization.kt
@@ -1,15 +1,12 @@
 package home.plugins
 
-import home.probes.LivenessProbe
 import home.probes.Probe
 import home.probes.ProbeStatus
-import home.probes.ReadinessProbe
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
-import io.ktor.server.application.*
-import io.ktor.server.request.*
 import io.ktor.server.routing.*
 
 fun Application.configureSerialization(probe: Probe) {
@@ -26,50 +23,12 @@ fun Application.configureSerialization(probe: Probe) {
             call.respond(status = HttpStatusCode.OK, message = "-> ALIVE from: $hostname")
         }
 
-        post("/probe/liveness/") {
-            runCatching {
-                when (val probeStatus = call.receive<LivenessProbe>().liveness) {
-                    ProbeStatus.SUCCESS, ProbeStatus.FAILED -> {
-                        val statusCode = if (probe.liveness == probeStatus) {
-                            HttpStatusCode.NotModified
-                        } else {
-                            probe.liveness = probeStatus
-                            HttpStatusCode.Accepted
-                        }
-
-                        call.respond(status = statusCode, message = "success")
-                    }
-                }
-            }.getOrElse {
-                call.respond(status = HttpStatusCode.BadRequest, message = "failed")
-            }
-        }
-
         get("/probe/liveness/") {
             val statusCode = when (probe.liveness) {
                 ProbeStatus.SUCCESS -> HttpStatusCode.OK
                 ProbeStatus.FAILED -> HttpStatusCode.ServiceUnavailable
             }
             call.respond(status = statusCode, message = "${probe.liveness}")
-        }
-
-        post("/probe/readiness/") {
-            runCatching {
-                when (val probeStatus = call.receive<ReadinessProbe>().readiness) {
-                    ProbeStatus.SUCCESS, ProbeStatus.FAILED -> {
-                        val statusCode = if (probe.readiness == probeStatus) {
-                            HttpStatusCode.NotModified
-                        } else {
-                            probe.readiness = probeStatus
-                            HttpStatusCode.Accepted
-                        }
-
-                        call.respond(status = statusCode, message = "success")
-                    }
-                }
-            }.getOrElse {
-                call.respond(status = HttpStatusCode.BadRequest, message = "failed")
-            }
         }
 
         get("/probe/readiness/") {
